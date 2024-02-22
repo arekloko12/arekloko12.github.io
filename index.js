0779e9cb44bac7df0be272982ecc2243508f38dd
arekloko <129875653+arekloko12@users.noreply.github.com>
2024-02-22 21:33:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+burgerbtn = document.querySelector(".burger");
+menu = document.querySelector(".menu");
+
+burgerbtn.addEventListener("click", () => {
+  burgerbtn.classList.toggle("active");
+  menu.classList.toggle("active");
+});
